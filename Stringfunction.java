be2bb2f1bfee5b1dbce30b17461a2ad46547e287
swethaprathipati <33229649+swethaprathipati@users.noreply.github.com>
2020-07-26 00:47:41 +0530

    Created String function using lambdas and streams.

---
+++ Stringfunction.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+public class Stringfunction {
+         public static void main(String[] args) {
+
+        List<String> words = Arrays.asList("pen", "custom", "orphanage","forest", "bubble", "butterfly");
+
+        List<String> result = words.stream().filter(word -> word.length() > 5).collect(Collectors.toList());
+
+        result.forEach(word -> System.out.println(word));
+    }
+}
